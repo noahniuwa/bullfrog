25150d6ef167bb65c7bdc25bf772dfa6d0965b75
noahniuwa <noahmaizels@protonmail.com>
2022-09-20 23:42:52 +0700

    animations

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -34,29 +34,29 @@
         <span class="sr-only">Logo</span>
         <img class="h-10 w-auto" src={logo} alt="">
       </a>
-      <img on:click={handleClick} class="border-2 cursor-pointer border-black rounded absolute w-8 right-4 top-6 md:hidden" src={burgerIcon} alt="menu">
+      <img on:click={handleClick} class="border-2 cursor-pointer border-amber-900 rounded absolute w-8 right-4 top-6 md:hidden" src={burgerIcon} alt="menu">
 
       <div class="transition-opacity  md:hidden block absolute right-0 top-0 z-30 mt-3 w-screen max-w-xs px-2 md:opacity-0 {menuOpen ? "opacity-100" : "opacity-0 hidden"} ">
-        <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+        <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-amber-900 ring-opacity-5">
       
           <div class="z-40 relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
             <div>
-              <img on:click={handleClick} class="h-8 cursor-pointer border-black border rounded absolute right-2 top-2" alt="close icon" src={xIcon}>
+              <img on:click={handleClick} class="h-8 cursor-pointer border-amber-900 border rounded absolute right-2 top-2" alt="close icon" src={xIcon}>
 
             </div>
             <a href="#about" class="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50">
-              <p class="text-base font-quicksand text-gray-900">ABOUT</p>
-              <p class="mt-1 text-sm text-gray-500">Get to know the Beekeeper team and philosophy.</p>
+              <p class="text-base font-quicksand text-amber-900">ABOUT</p>
+              <p class="mt-1 text-sm text-amber-500">Get to know the Hive team and philosophy.</p>
             </a>
     
             <a href="#services" class="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50">
-              <p class="text-base font-quicksand text-gray-900">SERVICES</p>
-              <p class="mt-1 text-sm text-gray-500">Beekeeper's menu of services to help take your business to the next level.</p>
+              <p class="text-base font-quicksand text-amber-900">SERVICES</p>
+              <p class="mt-1 text-sm text-amber-500">Hive's menu of services to help take your business to the next level.</p>
             </a>
     
             <a href="#contact" class="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50">
-              <p class="text-base font-quicksand text-gray-900">CONTACT</p>
-              <p class="mt-1 text-sm text-gray-500">Get in touch to set up a consultation.</p>
+              <p class="text-base font-quicksand text-amber-900">CONTACT</p>
+              <p class="mt-1 text-sm text-amber-500">Get in touch to set up a consultation.</p>
             </a>
      
           </div>
