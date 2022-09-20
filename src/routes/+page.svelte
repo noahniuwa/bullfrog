409fa254e2153b5f061c794f5e4536d1f0a23fc5
noahniuwa <noahmaizels@protonmail.com>
2022-09-20 21:59:12 +0700

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,14 +30,14 @@
 	
 
 </section>
-<section>
+<section class=" bg-4">
 	<!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-light-honey">
+<div class="font-quicksand bg-honeycomb">
   <div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
     <div class="text-center">
-      <h2 class="text-lg font-semibold text-indigo-600">Pricing</h2>
-      <p class="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">Take control of your team.</p>
-      <p class="mx-auto mt-5 max-w-xl text-xl text-gray-500">Start building for free, then add a site plan to go live. Account plans unlock additional features.</p>
+      <h2 class="text-lg font-semibold text-amber-900">Pricing</h2>
+      <p class="mt-1 text-4xl font-bold tracking-tight text-amber-900 sm:text-5xl lg:text-6xl">Global, experienced, multidisciplinary.</p>
+      <p class="mx-auto mt-5 max-w-xl text-xl text-amber-900">Our team of professionals has years of in-depth experience with communications work at fast paced tech and web3 startups. We specialize in <strong>community managment</strong>, <strong>token management</strong>, <strong>web development</strong>, <strong>content creation</strong>, and Chinese to English <strong>localization</strong></p>
     </div>
   </div>
 </div>
@@ -158,7 +158,7 @@
 
 </section>
 <style>	
- 
+
 	.animated-bg {
 		background-color: #FECE2F;
 background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 3000 1800'%3E%3Cdefs%3E%3Cpath id='a' fill='%2378350f' fill-opacity='.7' d='M4002.8.3C3620.4 14 3647 280.9 3371.4 340.6s-433.8-225-723.3-111.8-339.6 465.3-489.3 557.8c-149.7 92.5-562.5-20.8-728.1 120.4s-111.8 539.2-238.8 700.2-555.5 132.2-679.3 353.4c-123.9 221.2 108.6 547.6 38.2 762.1S48 3037.7 10.9 3286.3s307.8 453.1 316.8 713.7S-60.6 4385.1 8 4698.2s414.7 262.9 537.9 565.7c123.2 302.8-228 480.4-36 771s456.2 107.4 673 347 18.8 538 247.8 711.1c229 173.1 443.6-30.5 728.1 120.4s143.1 452.9 483 555.6 409.8-153.1 715.8-112c306 41.1 329.7 354.2 645.2 342.7s302.7-293.2 631.4-340.3c328.7-47.1 496 198.7 723.3 111.8s343.6-468.3 477.1-550.7c133.6-82.5 566.4 21.2 735.6-123.8 169.2-145 110.8-536.1 243.5-704 132.6-167.8 557.7-136.8 679.3-353.4s-99.6-579.6-38.2-762.1c61.4-182.5 507.3-353.9 539.9-563.6 32.6-209.7-311.2-506.8-316.8-713.7-5.7-206.8 358.7-509.6 319-701.6-39.7-192-470.7-390.9-537.3-562.3-66.6-171.4 163.6-571.8 36-771-127.6-199.2-504.6-179.3-661-345.2s-7-522.6-259.8-712.8-519-4.7-740.2-127.3-197.7-463.1-470.8-548.7-534.7 88.1-719.4 55S4290-10 4002.8.3z' /%3E%3C/defs%3E%3Cg transform='translate(0 120)' %3E%3Cg transform='translate(-2500 200) scale(1 .4)' stroke='%2378350f' stroke-width='1'%3E%3Cg%3E%3Cuse href='%23a' y='-100'%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' dur='52s' begin='-17s' values='0 4000 4000%3B360 4000 4000' %3E%3C/animateTransform%3E%3C/use%3E%3Cuse href='%23a' x='-100' y='150'%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' begin='-5s' dur='48.3s' values='0 4000 4000%3B360 4000 4000' %3E%3C/animateTransform%3E%3C/use%3E%3Cuse href='%23a' x='50' y='100'%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' dur='44.7s' values='0 4000 4000%3B360 4000 4000' %3E%3C/animateTransform%3E%3C/use%3E%3Cuse href='%23a' x='100'%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' begin='-14s' dur='40.9s' values='0 4000 4000%3B360 4000 4000' %3E%3C/animateTransform%3E%3C/use%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' dur='50s' values='360 4000 4000%3B0 4000 4000'%3E%3C/animateTransform%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
@@ -176,5 +176,12 @@ background-size: cover;
 		background-color: #78350f;
 		background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 100 1000 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='21%25' cy='94%25' fx='40%25' fy='25%25' r='100%25'%3E%3Cstop offset='0' stop-color='%23FECE2F' stop-opacity='1' /%3E%3Cstop offset='.1' stop-color='%23FECE2F' stop-opacity='.9' /%3E%3Cstop offset='.2' stop-color='%23FECE2F' stop-opacity='.75' /%3E%3Cstop offset='.4' stop-color='%23FECE2F' stop-opacity='.6' /%3E%3Cstop offset='.65' stop-color='%23FECE2F' stop-opacity='.4' /%3E%3Cstop offset='.9' stop-color='%23FECE2F' stop-opacity='.15' /%3E%3Cstop offset='1' stop-color='%23FECE2F' stop-opacity='0' /%3E%3C/radialGradient%3E%3ClinearGradient id='o' gradientUnits='objectBoundingBox' x1='0' y1='0' x2='0' y2='1'%3E%3Cstop offset='0' stop-color='%23070014' stop-opacity='0' /%3E%3Cstop offset='.1' stop-color='%23FECE2F' stop-opacity='0.1' /%3E%3Cstop offset='1' stop-color='%23FECE2F' /%3E%3C/linearGradient%3E%3Cg id='b'%3E%3Cg transform='rotate(30 0 0)' style='transform-origin:center'%3E%3CanimateTransform attributeName='transform' type='rotate' from='0 0 0' to='20 0 0' keyTimes='0%3B 1' calcMode='spline' keySplines='0 0 .3 1' dur='12s' repeatCount='1' fill='freeze'/%3E%3Cg transform='translate(0 0)' style='transform-origin:center'%3E%3CanimateTransform attributeName='transform' type='translate' from='0 0' to='150 100' keyTimes='0%3B 1' calcMode='spline' keySplines='0 0 .3 1' dur='12s' repeatCount='1' fill='freeze'/%3E%3Cpath fill='url(%23a)' d='M34.1-49.9c121.6 165.5 108 399-40.9 549.3C-4.7 499.4-375 865-375 865l723 135 .4-642.4C347.3 162.3 214.4-1.6 34.1-49.9z' /%3E%3Cpath fill='none' stroke='url(%23o)' stroke-width='1' d='M34.1-49.9c121.6 165.5 108 399-40.9 549.3L-375 865' /%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/defs%3E%3Cg transform='rotate(0 0 0)' style='transform-origin:center'%3E%3Cg id='c' transform='translate(500 500)'%3E%3Cuse href='%23b' transform='rotate(0 0 0)' /%3E%3Cuse href='%23b' transform='rotate(45 0 0)' /%3E%3Cuse href='%23b' transform='rotate(90 0 0)' /%3E%3Cuse href='%23b' transform='rotate(135 0 0)' /%3E%3Cuse href='%23b' transform='rotate(180 0 0)' /%3E%3Cuse href='%23b' transform='rotate(225 0 0)' /%3E%3Cuse href='%23b' transform='rotate(270 0 0)' /%3E%3Cuse href='%23b' transform='rotate(315 0 0)' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 		background-size: cover;
+	}
+
+	.bg-4 {
+		background-color: #FECE2F;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 1000'%3E%3CradialGradient id='a' cx='.5' fx='0' cy='.2' fy='.69' r='.7' gradientTransform='skewX(25) skewY(20)'%3E%3Cstop offset='0' stop-color='%23FFF' stop-opacity='1' /%3E%3Cstop offset='1' stop-color='%23FFF' stop-opacity='0' /%3E%3C/radialGradient%3E%3ClinearGradient id='b' x1='1' x2='0' y1='1' y2='0'%3E%3Cstop offset='0' stop-color='%2378350F' stop-opacity='1' /%3E%3Cstop offset='1' stop-color='%23FECE2F' stop-opacity='1' /%3E%3C/linearGradient%3E%3CradialGradient id='c' cx='.36' fx='.8' cy='.65' fy='.25' r='.6' gradientTransform='skewX(10)'%3E%3Cstop offset='0' stop-color='%23FFF' stop-opacity='1' /%3E%3Cstop offset='1' stop-color='%23FECE2F' stop-opacity='0' /%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='2.8' fx='.84' cy='-2' fy='-2.4' r='2' gradientTransform='skewX(-50) skewY(70)'%3E%3Cstop offset='0' stop-color='%23FECE2F' stop-opacity='1' /%3E%3Cstop offset='1' stop-color='%23FECE2F' stop-opacity='0' /%3E%3C/radialGradient%3E%3Crect fill='url(%23b)' width='100%25' height='100%25' /%3E%3Cg %3E%3Crect fill='url(%23a)' width='100%25' height='100%25' /%3E%3Crect fill='url(%23d)' width='100%25' height='100%25' /%3E%3Crect fill='url(%23c)' width='100%25' height='100%25' /%3E%3C/g%3E%3C/svg%3E");
+
+background-size: cover;
 	}
 </style>
