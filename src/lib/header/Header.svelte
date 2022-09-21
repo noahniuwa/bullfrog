3630755b529dfa5577ff50977a0e651777676b4d
noahniuwa <noahmaizels@protonmail.com>
2022-09-21 18:29:08 +0700

    logo

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -23,16 +23,16 @@
           <img class="h-10 w-auto" src={logo} alt="">
         </a>
         <div class="align-middle items-center flex flex-row ml-10 space-x-6">
-          <a href="#about" class="border-b-2 duration-200 ease-in-out hover:scale-110  border-amber-900 text-2xl text-amber-900">ABOUT</a>
+          <a href="#about" class="duration-200 ease-in-out hover:scale-110  text-xl text-black">ABOUT</a>
 
-          <a href="#services" class="border-b-2 duration-200 ease-in-out hover:scale-110 border-amber-900 text-2xl text-amber-900">SERVICES</a>
+          <a href="#services" class=" duration-200 ease-in-out hover:scale-110  text-xl text-black">SERVICES</a>
 
-          <a href="#contact" class="border-b-2 duration-200 ease-in-out hover:scale-110 border-amber-900 text-2xl  text-amber-900 ">CONTACT</a>
+          <a href="#contact" class="duration-200 ease-in-out hover:scale-110 text-xl  text-black ">CONTACT</a>
           
-          <a href="#contact" class="duration-200 ease-in-out hover:scale-110 inline-block w-10 block rounded-md transition">
+          <a href="#contact" class="duration-200 ease-in-out hover:scale-110 inline-block w-6 block rounded-md transition">
             <img src={twitter}>
           </a>
-          <a href="#contact" class="duration-200 ease-in-out hover:scale-110 inline-block w-10 block rounded-md transition">
+          <a href="#contact" class="duration-200 ease-in-out hover:scale-110 inline-block w-6 block rounded-md transition">
             <img src={telegram}>            
           </a>
          
@@ -44,28 +44,28 @@
         <span class="sr-only">Logo</span>
         <img class="h-10 w-auto" src={logo} alt="">
       </a>
-      <img on:click={handleClick} class="border-2 cursor-pointer border-amber-900 rounded absolute w-8 right-4 top-6 md:hidden" src={burgerIcon} alt="menu">
+      <img on:click={handleClick} class="border-2 cursor-pointer border-black rounded absolute w-8 right-4 top-6 md:hidden" src={burgerIcon} alt="menu">
 
       <div class="transition-opacity duration-300 ease-in-out md:hidden block absolute right-0 top-0 z-30 mt-3 w-screen max-w-xs px-2 md:opacity-0 {menuOpen ? "opacity-100 " : "opacity-0 pointer-events-none"} ">
-        <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-amber-900 ring-opacity-5">
+        <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
       
           <div class="z-40 relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
             <div>
-              <img on:click={handleClick} class="h-8 cursor-pointer border-amber-900 border-2 rounded absolute right-2 top-2" alt="close icon" src={xIcon}>
+              <img on:click={handleClick} class="h-8 cursor-pointer border-black border-2 rounded absolute right-2 top-2" alt="close icon" src={xIcon}>
 
             </div>
             <a href="#about" class="-m-3 block rounded-md p-3 transition duration-200 ease-in-out hover:bg-gray-50">
-              <p class="text-base font-quicksand text-amber-900">ABOUT</p>
+              <p class="text-base font-quicksand text-black">ABOUT</p>
               <p class="mt-1 text-sm text-amber-500">Get to know the Hive team and philosophy.</p>
             </a>
     
             <a href="#services" class="-m-3 block rounded-md p-3 transition duration-200 ease-in-out hover:bg-gray-50">
-              <p class="text-base font-quicksand text-amber-900">SERVICES</p>
+              <p class="text-base font-quicksand text-black">SERVICES</p>
               <p class="mt-1 text-sm text-amber-500">Hive's menu of services to help take your business to the next level.</p>
             </a>
     
             <a href="#contact" class="-m-3 block rounded-md p-3 transition duration-200 ease-in-out hover:bg-gray-50">
-              <p class="text-base font-quicksand text-amber-900">CONTACT</p>
+              <p class="text-base font-quicksand text-black">CONTACT</p>
               <p class="mt-1 text-sm text-amber-500">Get in touch to set up a consultation.</p>
             </a>
 
